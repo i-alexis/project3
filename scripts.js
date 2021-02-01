49ba726ec355e548f14408d33e9b66a428ee2319
i-alexis <77300748+i-alexis@users.noreply.github.com>
2021-02-01 10:25:24 -0500

    alien generator first pass

---
+++ scripts.js
@@ -0,0 +1,10 @@
+/* numOfArms= number of arms lmao no shit
+    numOfLegs= take a guess.
+    bodyHeight adjusts for centipede + quadruped bodies; affects maximum of numOfArms on humanoid and quadruped, affects numOfLegs on centipede
+    bodyWidth 
+    bodyPlan = 1, 2, or 3
+        > 1=humanoid
+        > 2=quadruped
+        > 3=centipede
+*/
+
