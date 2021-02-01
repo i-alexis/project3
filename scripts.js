3af365042cbc0795e5842aaa73c7dfb1d00440a4
i-alexis <77300748+i-alexis@users.noreply.github.com>
2021-02-01 12:36:07 -0500

    added height + width slider functionality

--- scripts.js
+++ scripts.js
@@ -1,10 +1,27 @@
 /* numOfArms= number of arms lmao no shit
     numOfLegs= take a guess.
-    bodyHeight adjusts for centipede + quadruped bodies; affects maximum of numOfArms on humanoid and quadruped, affects numOfLegs on centipede
-    bodyWidth 
+    bodyHeight affects bottom value of legs
+    bodyWidth affects spacing of leg
     bodyPlan = 1, 2, or 3
-        > 1=humanoid
-        > 2=quadruped
-        > 3=centipede
+        > 1=biped
+        > 2=quadruped: change arms to display:none
 */
 
+var bodyHeight=250
+var bodyWidth=100
+var limbWidth=50
+
+$(function(){
+    $("#height").change(function(){
+        bodyHeight=($("#height").val()) + "px"
+        $("#body").css("height", bodyHeight)
+    })
+    
+    $("#width").change(function(){
+        bodyWidth=parseInt($("#width").val())
+        armPos=(bodyWidth+limbWidth)+"px"
+        $("#body").css("width", bodyWidth+"px")
+        $(".arm:after").css("left",armPos)
+        console.log(armPos)
+    })
+})
