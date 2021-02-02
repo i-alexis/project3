0a8ab3138969c6e0130a52ff2dcc70d12421bf56
i-alexis <77300748+i-alexis@users.noreply.github.com>
2021-02-02 11:45:02 -0500

    HE LIVES (Almost)

--- scripts.js
+++ scripts.js
@@ -1,34 +1,49 @@
-/* numOfArms= number of arms lmao no shit
-    numOfLegs= take a guess.
+/* numOfArms= number of arms lmao
+    numOfLegs= bro. take a guess
     bodyHeight affects bottom value of legs
     bodyWidth affects spacing of leg
     bodyPlan = 1, 2, or 3
         > 1=biped
         > 2=quadruped: change arms to display:none
 */
+var numOfArms   
+var bodyHeight=250
+var bodyWidth=150
+var limbWidth=45
+let bodyPlan;
 
+function adjustHeight(){
+    bodyHeight=($("#height").val()) + "px"
+    $("#body").css("height", bodyHeight)
+    console.log($("#body").css("top"))
+}
 
+function adjustWidth(){
+    bodyWidth=parseInt($("#width").val())
+    armPos=(bodyWidth)+"px"
+    legPos=(bodyWidth-limbWidth)+"px"
+    $("#body").css("width", bodyWidth+"px")
+    $("#arm2").css("left",armPos)
+    $("#leg2").css("left",legPos)
+    $("#head").css("left", (bodyWidth/2)-50)
+    console.log(armPos)
+}
 
+//HEIGHT/WIDTH SLIDERS
 $(function(){
-    var bodyHeight=250
-    var bodyWidth=100
-    var limbWidth=50
+    adjustWidth()
+    adjustHeight()
     $("#height").change(function(){
-        bodyHeight=($("#height").val()) + "px"
-        $("#body").css("height", bodyHeight)
+        adjustHeight()
     })
     
     $("#width").change(function(){
-        bodyWidth=parseInt($("#width").val())
-        armPos=(bodyWidth)+"px"
-        legPos=(bodyWidth-limbWidth)+"px"
-        $("#body").css("width", bodyWidth+"px")
-        $("#arm2").css("left",armPos)
-        $("#leg2").css("left",legPos)
-        console.log(armPos)
-        // armPos=(bodyWidth+limbWidth)+"px"
-        // $("#body").css("width", bodyWidth+"px")
-        // $("#arm2").css("left",armPos)
-        // console.log(armPos)
+        adjustWidth()
     })
+})
+
+
+$(function(){
+    
+    
 })
